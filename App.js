d29840e2b5bc57459d184f705d3ca450e141ac5f
José Manuel <expressmillionaire@hotmail.com>
2018-04-22 19:03:30 +0200

    correct set up

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open</Text>
       </View>
     );
   }
@@ -15,5 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
